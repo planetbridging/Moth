df86a59c697daba8d13b4fb82144656bd1908562
samuel watson <samuel@poolingdata.bstnma.fios.verizon.net>
2023-07-13 20:07:59 +1000

    bedrock

---
+++ engine.py
@@ -0,0 +1,39 @@
+from djitellopy import Tello
+import cv2
+import time
+import threading
+
+# Create a Tello instance
+tello = Tello()
+
+# Connect to the drone
+tello.connect()
+time.sleep(5)  # Wait for 5 seconds
+
+# Check the battery level
+print("Battery level is: %s" % tello.get_battery())
+
+# Start the video stream
+tello.streamon()
+
+# The drone needs to be in the air to take a picture
+#tello.takeoff()
+
+def take_picture():
+    # Take a photo
+    frame_read = tello.get_frame_read()
+    frame = frame_read.frame
+
+    cv2.imwrite("./pics/"+f"picture_{time.time()}.jpg", frame)
+
+    # Start another timer
+    threading.Timer(5.0, take_picture).start()
+
+# Start the first timer
+take_picture()
+
+    # Wait for 5 seconds
+    #time.sleep(5)
+
+# Land the drone
+#tello.land()
